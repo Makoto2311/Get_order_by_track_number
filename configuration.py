851a07059fafa2bad7a5212b782d4eca4d668179
Makoto2311 <minaweq@gmail.com>
2025-10-31 15:32:02 +0300

    Заливаю на гит

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE ="https://313e1b81-8218-416a-9637-12025e5ee161.serverhub.praktikum-services.ru/"
+DOC_PATH = "/docs/"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_PATH = "/api/v1/orders/track"
